162b24b1a4c486bd09cf448f51a03585b7376ab7
Zahar <@email>
2026-04-29 17:28:50 +0300

    Sheska navaibkodil

--- src/EduSpace.Api/Program.cs
+++ src/EduSpace.Api/Program.cs
@@ -23,8 +23,22 @@ builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
 
--- src/EduSpace.Data/models/AppUser.cs
+++ src/EduSpace.Data/models/AppUser.cs
@@ -6,5 +6,6 @@ public abstract class AppUser : IdentityUser
 {
     public string FullName { get; set; } = string.Empty;
     public string? AvatarPath { get; set; }
+    public string UserType { get; set; }
 }
 
